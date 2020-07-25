eee5403005f81ddd8a4eb075ecfd37eb9c828484
emikeda <emikeda.68@gmail.com>
2020-07-25 21:20:25 +0900

    モデルコース一覧php編集しました

--- archive-model.php
+++ archive-model.php
@@ -1,37 +1,46 @@
+<?php get_header('subpage'); ?>
 <main>
 
     <article>
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : ?>
-                <?php the_post(); ?>
+        <div class="archive_model_spot_wrap">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : ?>
+                    <?php the_post(); ?>
 
-                <article <?php post_class(); ?>>
+                    <article <?php post_class(); ?>>
 
-                    <h3 class="">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </h3>
+                        <div class="model_course_card model_fade">
+                            <?php
+                            $loop = CFS()->get('course_loop');
+                            foreach ($loop as $row) :
+                            ?>
+                                <!-- 画像： -->
+                                <img src="<?php echo $row['model_spot_img']; ?>">
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="model_text">
+                            <h3 class="">
+                                <?php the_title(); ?>
+
+                            </h3>
 
 
-                    <p>モデルコース説明テキスト：</br>
-                        <?php echo CFS()->get('iyashi_point'); ?>
-                    </p>
+                            <p class="model_card_description">
+                                <?php echo CFS()->get('model_info'); ?>
+                            </p>
+                            <a href="<?php the_permalink(); ?>">
+                                <button class="spot_more theme_color_dark">
+                                    モデルコース詳細へ
+                                    <i class="fas fa-chevron-right"></i>
+                                </button>
+                            </a>
+                        </div>
+                    </article>
 
-                    <ul>
-                        <?php
-                        $loop = CFS()->get('course_loop');
-                        foreach ($loop as $row) :
-                        ?>
-                            <li>画像：<img src="<?php echo $row['model_spot_img']; ?>"></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </article>
+                <?php endwhile; ?>
 
-            <?php endwhile; ?>
-
-        <?php endif; ?>
-
+            <?php endif; ?>
+        </div>
     </article>
 
 </main>
