804c63234fd825176a62a9b66c19b8678e1dc8f1
Yuki1196 <poli.sep.6@gmail.com>
2020-07-27 11:59:55 +0900

    GoogleMap Iframeにクラス追加(レスポンシブ用)

--- single-model.php
+++ single-model.php
@@ -1,146 +1,141 @@
 <!-- ▼ ヘッダー : 開始-->
 <?php get_header('subpage'); ?>
 <!-- ▲ ヘッダー : 終了-->
+<?php
+// 変数の定義と背景画像の配列
+$post_type = 'spot';
+$tax_name = 'spot_cat';
+$course_loopcount = 0;
+$background_img = ['awaiyashi_gyogun', 'awaiyashi_sudachi_flower3', 'awaiyashi_sudatiburi', 'awaiyashi_car'];
+?>
 
 <main>
+    <section class="container model_container_wrap">
+        <div class="model_pc_img_top">
+            <img src="<?php echo esc_url(get_theme_file_uri('image/model_pc_img.png')); ?>" alt="" />
+        </div>
 
-    <article>
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : ?>
                 <?php the_post(); ?>
 
-                <section class="container model_container_wrap">
-                    <article <?php post_class(); ?>>
-                        <div class="model_description">
-                            <!-- モデルコースタイトル -->
-                            <h2><?php the_title(); ?></h2>
-                            <!-- モデルコース説明文 -->
-                            <p><?php echo CFS()->get('model_info'); ?></p>
-                        </div>
+                <div class="model_description">
+                    <?php // <!-- モデルコースタイトル -->
+                    the_title('<h2>', '</h2>'); ?>
+                    <p><?php // <!-- モデルコース説明文 -->
+                        echo CFS()->get('model_info'); ?></p>
+                </div>
 
-                        <!-- スタート -->
-                        <!-- <div class="model_start_img">
+                <!-- スタート -->
+                <!-- <div class="model_start_img">
                             <img src="<?php echo esc_url(get_theme_file_uri('image/model_course_sample.jpg')); ?>" alt="" />
                         </div> -->
 
-                        <span class=" calm_color_dark model_circle_set model_big_circle">スタート！</span>
+                <span class="calm_color_dark model_circle_set model_big_circle">
+                    スタート！
+                </span>
 
-                        <!-- 道路 -->
-                        <span class="calm_color_dark road"> <img class="model_sp_img2" src="<?php echo esc_url(get_theme_file_uri('image/awaiyashi_gyogun.png')); ?>" alt="" /></span>
+                <?php
+                // <!-- ▼ コースループ : 開始-->
+                $loop = CFS()->get('course_loop');
+                foreach ($loop as $row) :
+                ?>
+                    <!-- 道路 -->
+                    <span class="calm_color_dark road">
+                        <?php if ($course_loopcount < 2) :
+                            echo '<img class="model_sp_img', $course_loopcount + 2, '" src="', esc_url(get_theme_file_uri('image/' . $background_img[2 * $course_loopcount] . '.png')), '" alt="" />';
+                        endif; ?>
+                    </span>
 
+                    <?php $spot_slug = $row['spot_slug']; ?>
+                    <?php $args = array(
+                        'post_type' => $post_type,
+                        'name'      => $spot_slug
+                    );
+                    $customPosts = get_posts($args);
+                    // <!-- ▼ spotループ : 開始-->
+                    if ($customPosts) :
+                        foreach ($customPosts as $post) :
+                            setup_postdata($post);
+                            $spot_cat = get_category_parent($post, $tax_name);
+                            $spot_cat_slug = $spot_cat->slug;
+                    ?>
+                            <!-- スポットカード-->
+                            <div class="pc_spot_more">
+                                <button class="spot_more <?php echo $spot_cat_slug; ?>_color_dark pc_spot_name1 <?php echo $spot_cat_slug; ?>_color_dark">
+                                    <?php the_title(); ?> <i class="fas fa-chevron-<?php echo $course_loopcount == 1 ? 'left' : 'right' ?>"></i>
+                                </button>
+                            </div>
 
-
-                        <ul>
-                            <?php
-                            $loop = CFS()->get('course_loop');
-                            foreach ($loop as $row) :
-                            ?>
-
-                                <?php $spot_slug = $row['spot_slug']; ?>
-
-                                <?php $args = array(
-                                    'post_type' => 'spot', //投稿タイプ名
-                                    'name' => $spot_slug
-                                );
-                                $customPosts = get_posts($args);
-                                if ($customPosts) :
-                                    foreach ($customPosts as $post) :
-                                        setup_postdata($post);
-                                        $loopcount++;
-                                ?>
-
-                                        <!-- スポットカード-1-->
-                                        <div class="pc_spot_more">
-                                            <button class="spot_more fun_color_dark pc_spot_name1 fun_color_dark">
-                                                <?php the_title(); ?><i class="fas fa-chevron-right"></i>
+                            <div class="model_spot_wrap spot_fade_in<?php echo $course_loopcount + 1; ?>" id="modelSpotCard<?php echo $course_loopcount + 1; ?>">
+                                <div class="model_spot_card <?php echo $spot_cat_slug; ?>_bdcolor_dark">
+                                    <div>
+                                        <?php // <!-- サムネイル -->
+                                        the_post_thumbnail(); ?>
+                                    </div>
+                                    <div class="model_text">
+                                        <?php // <!-- タイトル -->
+                                        the_title('<h3>', '</h3>'); ?>
+                                        <p class="model_card_description">
+                                            <?php // <!-- 癒しポイントテキスト -->
+                                            echo CFS()->get('iyashi_point'); ?>
+                                        </p>
+                                        <a href="<?php echo the_permalink(); // <!-- spot詳細へのリンク -->
+                                                    ?>">
+                                            <button class="spot_more calm_color_dark fun">
+                                                詳細はこちら <i class="fas fa-chevron-right"></i>
                                             </button>
-                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <p>該当スポットがありません。</p>
+                    <?php endif;
+                    wp_reset_postdata(); //クエリのリセット
+                    // <!-- ▲ spotループ : 終了-->
+                    ?>
 
-                                        <?php if ($loopcount <= 1) : ?>
-                                            <!-- ▼ サムネイル -->
-                                            <div class="model_spot_wrap spot_fade_in1" id="modelSpotCard1">
-                                                <div class="model_spot_card fun_bdcolor_dark">
-                                                <?php elseif ($loopcount == 2) : ?>
-                                                    <!-- ２件目以降の内容　-->
-                                                    <div class="model_spot_wrap spot_fade_in2" id="modelSpotCard2">
-                                                        <div class="model_spot_card fun_bdcolor_dark">
-                                                        <?php else : ?>
-                                                            <div class="model_spot_wrap spot_fade_in3" id="modelSpotCard3">
-                                                                <div class="model_spot_card fun_bdcolor_dark">
-                                                                <?php endif; ?>
-                                                                <div>
-                                                                    <?php the_post_thumbnail(); ?>
-                                                                </div>
-                                                                <!-- タイトル -->
-                                                                <div class="model_text">
-                                                                    <h3><?php the_title(); ?></h3>
+                    <!-- 道路 -->
+                    <span class="calm_color_dark road">
+                        <?php if ($course_loopcount < 2) :
+                            echo '<img class="model_sp_img', $course_loopcount + 4, '" src="', esc_url(get_theme_file_uri('image/' . $background_img[2 * $course_loopcount + 1] . '.png')), '" alt="" />';
+                        endif; ?>
+                    </span>
 
+                    <!-- 所要時間 -->
+                    <?php
+                    if (!empty($row['travel_time'])) {
+                        echo '<span class="calm_color_dark model_circle_set">';
+                        echo $row['travel_time'];
+                        echo '</span>';
+                    }
+                    ?>
 
-                                                                    <!-- ▼ 癒しポイントテキスト -->
-                                                                    <p class="model_card_description">
-                                                                        <?php echo CFS()->get('iyashi_point'); ?>
-                                                                    </p>
+                    <?php $course_loopcount++; ?>
+                <?php endforeach;
+                // <!-- ▲ コースループ : 終了-->
+                ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
 
-                                                                    <!-- ▼ spot詳細へのパーマリンク -->
-                                                                    <a href=" <?php echo the_permalink(); ?>"><button class="spot_more calm_color_dark fun">
-                                                                            詳細はこちら
-                                                                            <i class="fas fa-chevron-right"></i>
-                                                                        </button></a>
+        <!-- ゴール -->
+        <span class="calm_color_dark model_circle_set model_big_circle">ゴール！</span>
+        <div>
+            <img class="model_sp_img6" src="<?php echo esc_url(get_theme_file_uri('image/model_bottom_img.png')); ?>" alt="" />
+        </div>
 
-                                                                </div>
+        <div class="model_iframe_outline">
+            <?php echo CFS()->get('map_if'); ?>
+        </div>
 
-                                                                </div>
-                                                            </div>
-
-
-
-                                                        <?php endforeach; ?>
-                                                    <?php else : ?>
-                                                        <p>Sorry, no posts matched your criteria.</p>
-                                                    <?php endif;
-                                                wp_reset_postdata(); //クエリのリセット
-                                                    ?>
-
-                                                    <!-- 道路 -->
-                                                    <span class="calm_color_dark road">
-                                                        <img class="model_sp_img4" src="<?php echo esc_url(get_theme_file_uri('image/awaiyashi_sudachi_flower3.png')); ?>" alt="" />
-                                                    </span>
-
-                                                    <!-- 所要時間 -->
-                                                    <?php if ($loopcount <= 2) : ?>
-                                                        <span class="calm_color_dark model_circle_set">
-                                                            <?php
-                                                            if (!empty($row['travel_time'])) {
-                                                                echo '<li>所要時間', $row['travel_time'], '</li>';
-                                                            }
-                                                            ?>
-                                                        </span>
-                                                        <!-- 道路 -->
-                                                        <span class="calm_color_dark road">
-                                                            <img class="model_sp_img3" src="<?php echo esc_url(get_theme_file_uri('image/awaiyashi_sudatiburi.png')); ?>" alt="" />
-                                                        </span>
-
-                                                    <?php else : ?>
-                                                        <!-- ゴール -->
-                                                        <span class="calm_color_dark model_circle_set model_big_circle">ゴール！</span>
-                                                        <div>
-                                                            <img class="model_sp_img6" src="<?php echo esc_url(get_theme_file_uri('image/model_bottom_img.png')); ?>" alt="" />
-                                                        </div>
-
-                                                    <?php endif; ?>
-                                                <?php endforeach; ?>
-                        </ul>
-                    </article>
-                <?php endwhile; ?>
-            <?php endif; ?>
-
-
-            <div>
-                <?php echo CFS()->get('map_if');; ?>
-            </div>
-                </section>
-    </article>
-
+        <a href="<?php echo esc_url(get_post_type_archive_link('model')); ?>">
+            <button class="back_model_archive calm_color_dark">
+                モデルコース一覧へ
+            </button>
+        </a>
+    </section>
 </main>
 
 <!-- ▼ フッター : 開始-->
