be954aeee9fe8ae6d7e7a12a0ec775646fd9f313
harumakiprog05 <harumakiprog05@gmail.com>
2020-07-28 15:11:56 +0900

    no message

--- js/jQuery.js
+++ js/jQuery.js
@@ -68,6 +68,7 @@ $(".toppage_header #pcgnav").css("display", "none");
 
 $(window).scroll(function () {
     scrollVolume = $(this).scrollTop();
+    // console.log(scrollVolume);
 
 
     if ($(window).width() >= 1000) {
@@ -105,21 +106,58 @@ $(function () {
     let scrollposition = $(".site_about").offset().top; //スクロール先の要素を指定
     let speed = 500; //スクロールするスピード(小さいほど早い)
 
+
+
     button.click(function () {
         $("body, html").animate({ scrollTop: scrollposition }, speed);
 
     })
 })
 
-//ボタンの点滅
+// トップページの3つの丸をクリックするとページ内リンクするやつ
+//楽しい
 $(function () {
-    $self = $(".fa-chevron-down"); //点滅させるボタンを指定
+    let button = $(".top_category_set li:nth-child(1)") //ボタンとなる要素を指定
+    let scrollposition = $(".fun_category").offset().top; //スクロール先の要素を指定
+    let speed = 500; //スクロールするスピード(小さいほど早い)
 
-    setInterval(function () {
-        $self.fadeOut(1800).fadeIn(1800);
-    }, 0)
-});
 
+
+    button.click(function () {
+        // console.log(scrollposition);
+
+        $("body, html").animate({ scrollTop: scrollposition }, speed);
+
+    })
+})
+//静か
+$(function () {
+    let button = $(".top_category_set li:nth-child(2)") //ボタンとなる要素を指定
+    let scrollposition = $(".calm_category").offset().top;; //スクロール先の要素を指定
+    let speed = 500; //スクロールするスピード(小さいほど早い)
+
+    button.click(function () {
+        // console.log(scrollposition);
+
+        $("body, html").animate({ scrollTop: scrollposition }, speed);
+
+    })
+})
+
+//旨い
+$(function () {
+    let button = $(".top_category_set li:nth-child(3)") //ボタンとなる要素を指定
+    let scrollposition = $(".tasty_category").offset().top;; //スクロール先の要素を指定
+    let speed = 500; //スクロールするスピード(小さいほど早い)
+
+
+
+    button.click(function () {
+        // console.log(scrollposition);
+        $("body, html").animate({ scrollTop: scrollposition }, speed);
+
+    })
+})
 
 
 // カテゴリーごとのスポットスライドショー
